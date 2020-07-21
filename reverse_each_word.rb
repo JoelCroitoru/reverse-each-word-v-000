ed6e903c65c9dc512036817f00df57607539c9e2
Joel Croitoru <jcroitoru@gmail.com>
2020-07-21 19:03:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sentence = sentence.split(" ")
   reversed_sentence = []
-  sentence.each do |word|
+  sentence.collect do |word|
     reversed_sentence << word.reverse
   end
   reversed_sentence.join(" ")
